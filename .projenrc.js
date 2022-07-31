141579c2704ba8058b264cdbfc2a5e7d7848c099
Bryan Galvin <bcgalvin@gmail.com>
2022-07-31 07:58:37 -0700

    import k8s + match cdk8s-plus version

--- .projenrc.js
+++ .projenrc.js
@@ -5,48 +5,48 @@ const cdk8sPlusVersion = '2.0.0-rc.67';
 const constructsVersion = '10.0.5';
 
 const project = new cdk8s.ConstructLibraryCdk8s({
-	author: 'Bryan Galvin',
-	authorAddress: 'bcgalvin@gmail.com',
-	name: 'cdk8s-metaflow',
-	keywords: ['cdk8s', 'cdk', 'metaflow'],
-	repositoryUrl: 'https://github.com/bcgalvin/cdk8s-metaflow.git', // Dependencies
-	cdk8sVersion: cdk8sVersion,
-	deps: [`cdk8s-plus-24@${cdk8sPlusVersion}`],
-	devDeps: ['eslint-config-prettier', 'eslint-plugin-prettier', 'prettier', '@types/cfn-response'],
-	peerDeps: [`constructs@${constructsVersion}`],
-	constructsVersion: constructsVersion, // Release
-	defaultReleaseBranch: 'main',
-	release: false,
-	releaseToNpm: false,
-	githubOptions: {
-		pullRequestLint: false,
-	},
-	depsUpgrade: false,
-	dependabot: false,
-	pullRequestTemplate: false,
-	clobber: false,
-	readme: true,
-	mergify: true, // Testing & Linting
-	codeCov: true,
-	eslint: true,
-	eslintOptions: {
-		prettier: true,
-	},
-	prettier: true,
-	prettierOptions: {
-		settings: {
-			printWidth: 120,
-			trailingComma: 'all',
-			arrowParens: 'always',
-			singleQuote: true,
-		},
-	},
-	eslintOptions: {
-		dirs: ['src'],
-		ignorePatterns: ['**/node_modules/**', '**/test/imports/**'],
-	},
-	gitignore: commonIgnore,
-	npmignore: commonIgnore,
+  author: 'Bryan Galvin',
+  authorAddress: 'bcgalvin@gmail.com',
+  name: 'cdk8s-metaflow',
+  keywords: ['cdk8s', 'cdk', 'metaflow'],
+  repositoryUrl: 'https://github.com/bcgalvin/cdk8s-metaflow.git', // Dependencies
+  cdk8sVersion: cdk8sVersion,
+  deps: [`cdk8s-plus-22@${cdk8sPlusVersion}`],
+  devDeps: ['eslint-config-prettier', 'eslint-plugin-prettier', 'prettier', '@types/cfn-response'],
+  peerDeps: [`constructs@${constructsVersion}`],
+  constructsVersion: constructsVersion, // Release
+  defaultReleaseBranch: 'main',
+  release: false,
+  releaseToNpm: false,
+  githubOptions: {
+    pullRequestLint: false,
+  },
+  depsUpgrade: false,
+  dependabot: false,
+  pullRequestTemplate: false,
+  clobber: false,
+  readme: true,
+  mergify: true, // Testing & Linting
+  codeCov: true,
+  eslint: true,
+  eslintOptions: {
+    prettier: true,
+  },
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 120,
+      trailingComma: 'all',
+      arrowParens: 'always',
+      singleQuote: true,
+    },
+  },
+  eslintOptions: {
+    dirs: ['src'],
+    ignorePatterns: ['**/node_modules/**', '**/test/imports/**'],
+  },
+  gitignore: commonIgnore,
+  npmignore: commonIgnore,
 });
 
 project.testTask.prependExec(`cd ./test && cdk8s import k8s --language typescript`);
