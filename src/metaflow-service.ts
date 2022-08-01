9e78a0c763e1fbd58cc511160d7e917fcdaaa2ad
Bryan Galvin <bcgalvin@gmail.com>
2022-07-31 18:21:44 -0700

    add deployment to metaflow-service construct

--- src/metaflow-service.ts
+++ src/metaflow-service.ts
@@ -1,11 +1,12 @@
 import * as kplus from 'cdk8s-plus-22';
-import { Protocol } from 'cdk8s-plus-22';
+import { LabelSelector, Protocol } from 'cdk8s-plus-22';
 import { Construct } from 'constructs';
 import { KubeServiceAccount } from './imports/k8s';
 
 export interface MetaflowServiceProps {
   readonly serviceAccountName?: string;
   readonly serviceName?: string;
+  readonly image?: string;
   readonly metadataServicePort?: number;
   readonly upgradesServicePort?: number;
 }
@@ -14,12 +15,13 @@ export class MetaflowService extends Construct {
   constructor(scope: Construct, id: string, props?: MetaflowServiceProps) {
     super(scope, id);
 
-    const metadataServicePort = props?.metadataServicePort || 8080;
-    const upgradesServicePort = props?.upgradesServicePort || 8082;
     const serviceAccountName = props?.serviceAccountName || 'release-name-metaflow-service';
     const serviceName = props?.serviceName || 'metaflow-service';
+    const image = props?.image || 'public.ecr.aws/outerbounds/metaflow_metadata_service:2.2.4';
+    const metadataServicePort = props?.metadataServicePort || 8080;
+    const upgradesServicePort = props?.upgradesServicePort || 8082;
 
-    new KubeServiceAccount(this, 'metaflow-sa', {
+    const serviceAccount = new KubeServiceAccount(this, 'metaflow-sa', {
       metadata: {
         name: serviceAccountName,
         labels: {
@@ -65,5 +67,61 @@ export class MetaflowService extends Construct {
         },
       }),
     });
+
+    const deployment = new kplus.Deployment(this, 'metaflow-deployment', {
+      replicas: 1,
+      initContainers: [
+        {
+          name: 'db-migrations',
+          image: image,
+          command: ['/opt/latest/bin/python3', '/root/run_goose.py', '--only-if-empty-db'],
+          envVariables: {
+            MF_METADATA_DB_NAME: kplus.EnvValue.fromValue(''),
+            MF_METADATA_DB_PORT: kplus.EnvValue.fromValue('5432'),
+            MF_METADATA_DB_PSWD: kplus.EnvValue.fromValue(''),
+            MF_METADATA_DB_USER: kplus.EnvValue.fromValue(''),
+            MF_METADATA_DB_HOST: kplus.EnvValue.fromValue('release-name-postgresql'),
+          },
+        },
+      ],
+      containers: [
+        {
+          name: 'metaflow-service',
+          image: image,
+          command: ['/opt/latest/bin/python3', '-m', 'services.metadata_service.server'],
+          imagePullPolicy: kplus.ImagePullPolicy.IF_NOT_PRESENT,
+          liveness: kplus.Probe.fromHttpGet('/ping', { port: metadataServicePort }),
+          readiness: kplus.Probe.fromHttpGet('/ping', { port: metadataServicePort }),
+          port: metadataServicePort,
+          envVariables: {
+            MF_METADATA_DB_NAME: kplus.EnvValue.fromValue(''),
+            MF_METADATA_DB_PORT: kplus.EnvValue.fromValue('5432'),
+            MF_METADATA_DB_PSWD: kplus.EnvValue.fromValue(''),
+            MF_METADATA_DB_USER: kplus.EnvValue.fromValue(''),
+            MF_METADATA_DB_HOST: kplus.EnvValue.fromValue('release-name-postgresql'),
+          },
+        },
+      ],
+      metadata: {
+        name: serviceAccountName,
+        labels: {
+          'helm.sh/chart': 'metaflow-service-0.2.0',
+          'app.kubernetes.io/name': serviceName,
+          'app.kubernetes.io/instance': 'release-name',
+          'app.kubernetes.io/version': '2.2.4',
+          'app.kubernetes.io/managed-by': 'Helm',
+        },
+      },
+      serviceAccount,
+    });
+
+    deployment.select(
+      LabelSelector.of({
+        labels: {
+          'app.kubernetes.io/name': 'metaflow-service',
+          'app.kubernetes.io/instance': 'release-name',
+        },
+      }),
+    );
   }
 }
